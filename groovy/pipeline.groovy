82a35a7e3c3a4342034a3a8cfb651a9e9e61b83f
Nicocovi <nico0205@hotmail.com>
2018-10-15 16:31:28 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -71,7 +71,7 @@ node {
         
         stage("Push Documentation"){
 		LocalDateTime t = LocalDateTime.now();
-		formatted = t.toString().replace("T", " ").substring(0,LENGTH-4)
+		formatted = t.toString().replace("T", " ").substring(0,t.length()-4)
 		def datestring = " { \"date\":\""+formatted+"\"} "
 		echo "Date: ${datestring}"
 		def iterations = APP_SHORTLIST.size() / 6
