5fd20d6661d7eec741182376a1aa55328df98f85
Nicocovi <nico0205@hotmail.com>
2018-10-17 10:54:51 +0200

    Update pipeline.groovy

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -91,24 +91,24 @@ node {
 		formatted = (t.toString()).replace("T", " ")
 		formatted = formatted.substring(0,(formatted.length())-4)
 		def datestring = " { \"date\":\""+formatted+"\"} "
-		echo "Date: ${datestring}"
+		echo "APP_SHORTLIST: ${APP_SHORTLIST}"
 		//TODO:
 		//BUILD JSON
-		
 		def iterations = APP_SHORTLIST.size() / 6
 		for (i = 0; i <iterations; i++) {
 			//TODO: get object and only change runtime
 			def basicinfo = "\"id\": \"XXX\", \"name\": \"${APP_SHORTLIST[0+6*i]}\", \"owner\": \"XXX\", \"description\": \"XXX\", \"short_name\": \"XXX\", \"type\": \"XXX\""
 			def additionalinfo = ", \"status\": \"${APP_SHORTLIST[1+6*i]}\", \"url\": \"${APP_SHORTLIST[5+6*i]}\", \"instances\": \"${APP_SHORTLIST[2+6*i]}\" "
             		def runtime = " \"runtime\": {\"ram\": \"${APP_SHORTLIST[3+6*i]}\", \"cpu\": \"XXX\", \"disk\": \"${APP_SHORTLIST[4+6*i]}\", \"host_type\": \"cloudfoundry\" }"
-            		def jsonstring = "{"+basicinfo+""+additionalinfo+""+runtime+"}"            
+            		def jsonstring = "{"+basicinfo+""+additionalinfo+""+runtime+"}"
+			echo "JSONString: ${jsonstring}"
             		try {
-				callPost("http://localhost:8080/endpoint/lastUpdateOfCrawler", datestring)
 				callPost("http://192.168.99.100:9123/update/microservice", jsonstring)
                 	} catch(e) {
                    		// if no try and catch: jenkins prints an error "no content-type" but post request succeed
 			}
 		}
+		callPost("http://localhost:8080/endpoint/lastUpdateOfCrawler", datestring)
         }
 
 }
