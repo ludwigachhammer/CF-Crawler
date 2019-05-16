14b0607bd117ea10fced270ddb26cf050db84dde
ludwigachhammer <38220221+ludwigachhammer@users.noreply.github.com>
2019-05-16 09:27:09 +0200

    Update testcrawler.groovy

--- groovy/testcrawler.groovy
+++ groovy/testcrawler.groovy
@@ -127,7 +127,12 @@ node {
 			def additionalinfo = " \"status\": \"${APPS_TO_UPDATE[4+9*i]}\", \"url\": \"${APPS_TO_UPDATE[3+9*i]}\", \"lastUpdate\":\"${tAsString}\", "
             		def runtime = " \"runtime\": {\"ram\": \"${APPS_TO_UPDATE[7+9*i]}\", \"cpu\": \"${APPS_TO_UPDATE[6+9*i]}\", \"disk\": \"${APPS_TO_UPDATE[8+9*i]}\", \"instances\": \"${APPS_TO_UPDATE[2+9*i]}\", \"host_type\": \"cloudfoundry\" }"
             		def jsonstring = "{"+basicinfo+""+additionalinfo+""+runtime+"}"
+			echo "basicinfo: ${basicinfo}"
+			echo "additionalinfo: ${additionalinfo}"
+			echo "runtime: ${runtime}"
 			echo "JSONString: ${jsonstring}"
+			def flatJSON = "{\"id\":\"${APPS_TO_UPDATE[0+9*i]}\",\"name\": \"${APPS_TO_UPDATE[1+9*i]}\", \"status\": \"${APPS_TO_UPDATE[4+9*i]}\", \"url\": \"${APPS_TO_UPDATE[3+9*i]}\", \"lastUpdate\":\"${tAsString}\", \"ram\": \"${APPS_TO_UPDATE[7+9*i]}\", \"cpu\": \"${APPS_TO_UPDATE[6+9*i]}\", \"disk\": \"${APPS_TO_UPDATE[8+9*i]}\", \"instances\": \"${APPS_TO_UPDATE[2+9*i]}\", \"host_type\": \"cloudfoundry\"}"
+			echo "flatJSON: ${flatJSON}" 
 			try{
 				//callPost("http://192.168.99.100:8080/update/microservice", jsonstring)
 				callPost("http://localhost:8025/update/microservice", jsonstring)
