228c02c2e065d34568e24acf5f4fb8dc33bca88d
ludwigachhammer <38220221+ludwigachhammer@users.noreply.github.com>
2019-06-26 14:04:52 +0200

    Update CF-API.groovy

--- CF-API.groovy
+++ CF-API.groovy
@@ -1,2 +1,7 @@
-def data = new URL(https://login.run.pivotal.io/passcode).getText()
-echo "${data}"
+node {
+
+  stage ("get passcode"){
+    def data = new URL("https://login.run.pivotal.io/passcode").getText()
+    echo "${data}"
+  }                 
+}
